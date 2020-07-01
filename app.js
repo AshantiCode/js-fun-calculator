42f3004dabd169defc27a0663f940933154d2fe1
Ashanti Haring <ashanti.haring@gmail.com>
2020-07-01 22:37:02 +0200

    finalize app

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 document.getElementById("calculator").onchange = function () {
   // get input values, convert strings to numbers
-  let totalBill = Number(document.getElementById("billInput").value);
+  let bill = Number(document.getElementById("billInput").value);
   let tipPercent = Number(document.getElementById("tipInput").value);
   let splitByPerson = Number(document.getElementById("splitInput").value);
 
-  // calculations of tip and bill per person
-  let tipValue = (totalBill / 100) * tipPercent;
-  let billPerPerson = (Math.round(((totalBill + tipValue) / splitByPerson) * 100) / 100).toFixed(2);
+  // calculations of tip and bill, bill per person
+  let tipValue = (bill / 100) * tipPercent;
+  let billPerPerson = (Math.round(((bill + tipValue) / splitByPerson) * 100) / 100).toFixed(2);
   let tipEach = (Math.round((tipValue / splitByPerson) * 100) / 100).toFixed(2);
-
+  let totalBill = bill + tipValue;
+  console.log("Total Bill: ", totalBill);
   // output input values and math calc to screen
   document.getElementById("tipOutput").innerHTML = tipPercent + "%";
   document.getElementById("splitOutput").innerHTML = splitByPerson;
-  document.getElementById("newBill").innerHTML = billPerPerson;
-  document.getElementById("tipEach").innerHTML = tipEach;
+  document.getElementById("newBill").innerHTML = billPerPerson + "€";
+  document.getElementById("tipEach").innerHTML = tipEach + "€";
+  document.getElementById("totalBill").innerHTML = totalBill + "€";
 };
