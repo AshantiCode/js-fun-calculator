ccb1370dce7ed757618b18852e8167abb8aee755
Ashanti Haring <ashanti.haring@gmail.com>
2020-07-01 19:49:48 +0200

    add math to round number to two diggits

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 document.getElementById("calculator").onchange = function () {
+  // get input values, convert strings to numbers
   let totalBill = Number(document.getElementById("billInput").value);
   let tipPercent = Number(document.getElementById("tipInput").value);
   let splitByPerson = Number(document.getElementById("splitInput").value);
 
+  // calculations of tip and bill per person
   let tipValue = (totalBill / 100) * tipPercent;
-  let billPerPerson = Math.floor((totalBill + tipValue) / splitByPerson);
+  let billPerPerson = (Math.round(((totalBill + tipValue) / splitByPerson) * 100) / 100).toFixed(2);
+  let tipEach = Math.floor(tipValue / splitByPerson);
 
+  // output input values and math calc to screen
   document.getElementById("tipOutput").innerHTML = tipPercent + "%";
   document.getElementById("splitOutput").innerHTML = splitByPerson;
   document.getElementById("newBill").innerHTML = billPerPerson;
+  document.getElementById("tipEach").innerHTML = tipEach;
 };
